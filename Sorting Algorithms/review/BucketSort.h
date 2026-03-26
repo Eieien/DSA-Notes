4a87a3527b48afc9eea3481580fc3a9aac0e6c36
Eien <depedronico02@gmail.com>
2026-03-26 12:09:32 +0800

    review

--- Sorting Algorithms/review/BucketSort.h
+++ Sorting Algorithms/review/BucketSort.h
@@ -4,115 +4,108 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<math.h>
+#include<limits.h>
 
-#define SENTINAL 999
+int getBucketRange(int numBuckets, int max, int min){
+    return ((max - min) / numBuckets)  + 1;
+}
+
+int hash(int range, int min, int val){
+    return ((val - min) / range);
+}
 
 void displayBuckets(int** buckets, int numBuckets, int len){
-    for(int i = 0; i < numBuckets; i++){
-        printf("[%d]", i );
 
-        for(int j = 0 ; j < len && buckets[i][j] != SENTINAL; j++){
+    for(int i = 0; i < numBuckets; i++){
+        printf("%d: ", i);
+        for(int j = 0; j < len && buckets[i][j] != INT_MAX; j++){
             printf("%d ", buckets[i][j]);
         }
         printf("\n");
     }
 }
 
-int** InitializeBuckets(int len, int numBuckets){
+
+int** createBuckets(int numBuckets, int len){
 
     int** buckets = (int**)malloc(sizeof(int*) * numBuckets);
 
     for(int i = 0; i < numBuckets; i++){
         buckets[i] = (int*)malloc(sizeof(int) * len);
     }
-    
+
     for(int i = 0; i < numBuckets; i++){
         for(int j = 0; j < len; j++){
-            buckets[i][j] = SENTINAL;
+            buckets[i][j] = INT_MAX;
         }
     }
 
     return buckets;
-}
 
-void insertBucket(int** buckets, int val, int key, int len){
-    int i;
-    for(i = 0; buckets[key][i] != SENTINAL && i < len ; i++){}
-    buckets[key][i] = val;
+} 
 
-}
+void insertionSortBucket(int* bucket, int len){
 
-int bucketRange(int max, int min, int numBuckets){
-    return ((max - min) / numBuckets) + 1;
-}
-
-int bucketIdx(int num, int min, int bucketRange){
-    return (num - min) / bucketRange;
-}
-
-void freeBuckets(int** buckets, int numBuckets){
-    
-    for(int i = 0; i < numBuckets; i++){
-        free(buckets[i]);
-    }
-
-    free(buckets);
-}
-
-void SortBucket(int* bucket, int len){
-
-    for(int i = 1; i < len; i++){
-        
+    for(int i = 1; i < len && bucket[i] != INT_MAX; i++){
+        int j = i - 1;
         int key = bucket[i];
-        int j = i;
 
-        while(j > 0 && bucket[j - 1] > key){
-            bucket[j] = bucket[j - 1];
+        while(j >= 0 && bucket[j] > key){
+            bucket[j + 1] = bucket[j];
             j--;
         }
-        bucket[j] = key;
+
+        bucket[j + 1] = key;
+
+    }
+
+}
+
+void insertItem(int* bucket, int len, int val){
+    int i;
+    for(i = 0; i < len && bucket[i] != INT_MAX; i++){}
+    if(i < len){
+        bucket[i] = val;
     }
 
 }
 
 void BucketSort(int arr[], int len){
-
     int numBuckets = sqrt(len);
-    int** buckets = InitializeBuckets(len, numBuckets);
+    int** buckets = createBuckets(numBuckets, len);
 
     int min = arr[0], max = arr[0];
-    
+
     for(int i = 0; i < len; i++){
-        if(min > arr[i]){
-            min = arr[i];
-        }
-        
         if(max < arr[i]){
             max = arr[i];
         }
-    }
-    int range = bucketRange(max, min, numBuckets);
 
-    for(int i = 0; i < len; i++){
-        int key = bucketIdx(arr[i], min, range);
-        insertBucket(buckets, arr[i], key, len);
+        if(min > arr[i]){
+            min = arr[i];
+        }
     }
-    displayBuckets(buckets, numBuckets, len);
+
+    int range = getBucketRange(numBuckets, max, min);
+    
+    for(int i = 0; i < len; i++){
+        int key = hash(range, min, arr[i]);
+        insertItem(buckets[key], len, arr[i]);
+    }
+
 
     for(int i = 0; i < numBuckets; i++){
-        SortBucket(buckets[i], len);
+        insertionSortBucket(buckets[i], len);
     }
-
+    // displayBuckets(buckets, numBuckets, len);
     int k = 0;
     for(int i = 0; i < numBuckets; i++){
-        for(int j = 0; j < len && buckets[i][j] != SENTINAL; j++){
+        for(int j = 0; j < len && buckets[i][j] != INT_MAX; j++){
             arr[k++] = buckets[i][j];
         }
     }
 
-    displayBuckets(buckets, numBuckets, len);
-    freeBuckets(buckets, numBuckets);
-    
 }
+
 
 #endif
--- Sorting Algorithms/review/review.c
+++ Sorting Algorithms/review/review.c
@@ -15,15 +15,15 @@ int main(){
     // int arr[5] = {64, 25, 12, 22, 11};
     // int arr[4] = {38, 27, 43, 10};
     // int arr[5] = {10, 80, 30, 90, 40};
-    int arr[8] = {2, 5, 3, 0, 2, 3, 0, 3};
+    // int arr[8] = {2, 5, 3, 0, 2, 3, 0, 3};
     // int arr[8] = {5, 3, 8, 4, 2, 7, 1, 10};
-    // int arr[8] = {29, 25, 3, 49, 9, 37, 21, 43};
+    int arr[8] = {29, 25, 3, 49, 9, 37, 21, 43};
     // int arr[9] = {22, 11, 25, 12, 45, 34, 90, 88, 64};
     int len = sizeof(arr) /sizeof(arr[0]);
     
     // insertionSort(arr, len);
     // GnomeSort(arr, len);
-    // BucketSort(arr, len);
+    BucketSort(arr, len);
     // StrandSort(arr, len);`
     // OfflineTournamentSort(arr, len);
     // ShellSort(arr, len);
@@ -31,7 +31,7 @@ int main(){
     // CountingSort(arr, len);
     // BubbleSort(arr, len);
     // CombSort(arr, len);
-    MergeSort(arr, 0, len - 1);
+    // MergeSort(arr, 0, len - 1);
     // RadixSort(arr, len);
     // QuickSort(arr, 0, len - 1);
     display(arr,len);
